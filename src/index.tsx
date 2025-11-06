a5294e47038e74c013ffb3355048eab2194daec7
Matthijs van den Bosch <mattievdboss@hotmail.com>
2025-11-06 18:47:08 +0100

    Fix import order and set THREE global for exporter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as THREE from "three";
-(window as any).THREE = THREE;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as THREE from "three";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -10,27 +12,31 @@ import reportWebVitals from "./reportWebVitals";
 import { Bzip2 } from "./rs/compression/Bzip2";
 import { Gzip } from "./rs/compression/Gzip";
 
+// Make THREE global
+(window as any).THREE = THREE;
+
 Bzip2.initWasm();
 Gzip.initWasm();
 
 window.wallpaperPropertyListener = {
-    applyGeneralProperties: (properties: any) => {
-        if (properties.fps) {
-            window.wallpaperFpsLimit = properties.fps;
-        }
-    },
+  applyGeneralProperties: (properties: any) => {
+    if (properties.fps) {
+      (window as any).wallpaperFpsLimit = properties.fps;
+    }
+  },
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
-    // <React.StrictMode>
-    <BrowserRouter>
-        <MapViewerApp />
-    </BrowserRouter>,
-    // </React.StrictMode>,
+  // <React.StrictMode>
+  <BrowserRouter>
+    <MapViewerApp />
+  </BrowserRouter>
+  // </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
