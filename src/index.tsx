9f2029df687659aebfcd2bb9d24f5b47ec37f70c
Matthijs van den Bosch <mattievdboss@hotmail.com>
2025-11-06 18:49:19 +0100

    Clean up imports and fix global THREE assignment

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
 import * as THREE from "three";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as THREE from "three";
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import "./index.css";
 import MapViewerApp from "./mapviewer/MapViewerApp";
 import reportWebVitals from "./reportWebVitals";
@@ -29,14 +25,13 @@ window.wallpaperPropertyListener = {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
     <MapViewerApp />
   </BrowserRouter>
-  // </React.StrictMode>,
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
